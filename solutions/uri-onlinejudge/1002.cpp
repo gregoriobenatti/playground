9f3a901bdc8b933d93980a0f84d7ee0e848e6643
Gregório Benatti <gregoriobenatti@gmail.com>
2016-09-27 12:18:33 -0300

    Update 1002.cpp

--- solutions/uri-onlinejudge/1002.cpp
+++ solutions/uri-onlinejudge/1002.cpp
@@ -1,22 +1,15 @@
 // Gregorio Benatti - URI Online Judge | 1001 - Area of a Circle
 
-#include<iostream>
-#include<iomanip>
+#include <iostream>
 
 using namespace std;
 
-int main()
-{
-    const float PI = 3.14159;
-    float A = 0, R = 0;
-    const int d = 4;
-
-    cin >> R;
-    A = (PI * (R*R));
-
-    cout.setf(ios::fixed, ios::floatfield);
-    cout.precision(d);
-    cout << "A=" << A << endl;
-
+int main(){
+    const double PI = 3.14159;
+    double R;
+    
+    scanf("%lf", &R);
+    printf("A=%.4f\n", R * R * PI);
+    
     return 0;
 }
